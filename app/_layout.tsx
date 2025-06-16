65dc91bf2cd1b1601ed1720b41b79f43129017cc
Ankit Khanal <akhanal749@gmail.com>
2025-06-16 14:30:04 +0545

    Updated layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,27 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack, SplashScreen } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import "../global.css";
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { ActivityIndicator } from 'react-native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-  const [appIsReady, setAppIsReady] = useState(false);
 
-  useEffect(() => {
-    const prepare = async () => {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-      } catch (e) {
-        console.warn("Splash screen error:", e);
-      } finally {
-        setAppIsReady(true);
-      }
-    };
-
-    prepare();
-  }, []);
-
-  useEffect(() => {
-    if (loaded && appIsReady) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, appIsReady]);
-
-  if (!loaded || !appIsReady) {
-    return null;
+  if (!loaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#1f2937" />
+      </View>
+    );
   }
+
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
